db51c367b941a8babb5fb53c86d652f90275e81e
Santosh Kumar <santoshkr97531@gmail.com>
2025-06-17 16:45:20 +0530

    Create NumberSum.java

---
+++ NumberSum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class NumberSum{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        System.out .println(" Enter a Number ");
+        int a = sc.nextInt();
+       https://www.onlinegdb.com/#editor_1 int sum =0;
+        for(int i =0 ; i<=a;i++)
+        {
+           sum= sum + i ; 
+        }
+        
+       System.out.println(" The Sum of the Number is  " +sum );
+        
+    }
+}
