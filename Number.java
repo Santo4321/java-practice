1b9b08f1c8fe4e7f478acdb474e68d676032b2ca
Santosh Kumar <santoshkr97531@gmail.com>
2025-06-17 16:18:06 +0530

    Number.java

---
+++ Number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Number {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        System.out .println(" Enter a Number ");
+        
+        int a = sc.nextInt();
+        
+        if (a < 0)
+        {
+            System.out.println(" Negative Number ");
+            
+        }
+        
+        else 
+        {
+            System.out.println(" Postive Number ");
+        }
+        
+    }
+}
